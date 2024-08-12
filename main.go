a0b8ff3618f0dd384494b139ba8e33429c4c5f8c
sajjad salemi <salemi9sajjad@gamil.com>
2024-08-12 19:25:41 +0330

    first commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	sequencebits = 2
+	timebits     = 32
+
+	maxsequencebits = (1 << sequencebits) - 1
+	maxtimebits     = (1 << timebits) - 1
+
+	sequenceshift = timebits
+)
+
+type snowflake struct {
+	sequence int64
+	time     int64
+	mux      sync.Mutex
+}
+
+func newsnowflake() *snowflake {
+	return &snowflake{
+		sequence: 0,
+		time:     -1,
+	}
+}
+
+func (s *snowflake) generator() int64 {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
+	epoch := time.Date(2024, time.August, 1, 0, 0, 0, 0, time.UTC)
+	now := time.Now().UnixMilli() - epoch.UnixMilli()
+
+	if now > maxtimebits {
+		fmt.Println("Timestamp exceeds 32-bit range!")
+		return -1
+	}
+
+	if now == s.time {
+		s.sequence = (s.sequence + 1) & maxsequencebits
+		if s.sequence == 0 {
+			for now <= s.time {
+				now = time.Now().UnixMilli() - epoch.UnixMilli()
+			}
+		}
+	} else {
+		s.sequence = 0
+	}
+
+	s.time = now
+
+	id := (s.sequence << sequenceshift) | s.time
+
+	return id
+}
+
+func main() {
+	sr := newsnowflake()
+	id := sr.generator()
+	ids := fmt.Sprintf("%034b\n", id)
+	fmt.Println(ids)
+	
+}
